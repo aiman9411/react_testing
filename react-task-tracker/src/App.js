37cb1c5ecb99697c07703f0ff4a3a1b755ffc291
Aiman Nazmi <aimannazmi59@gmail.com>
2022-02-19 11:15:51 +0800

    add useState hook

--- react-task-tracker/src/App.js
+++ react-task-tracker/src/App.js
@@ -1,12 +1,29 @@
 import Header from './components/Header';
 import Tasks from './components/Tasks';
+import {useState} from 'react'
 
 function App() {
+      
+  const [tasks, setTasks] = useState([
+    {
+        id: 1,
+        text: 'Doctors Appointment',
+        day: 'Feb 5th at 2.30pm',
+        reminder: false,
+    },
+
+    {
+        id: 2,
+        text: 'Grocery Shopping',
+        day: 'Mar 21st at 4.00pm',
+        reminder: false,
+    }
+])
  
   return (
     <div className="container">
       <Header />
-      <Tasks />
+      <Tasks tasks={tasks} />
     </div>
   );
 }
--- react-task-tracker/src/components/Tasks.js
+++ react-task-tracker/src/components/Tasks.js
@@ -1,24 +1,8 @@
-const tasks = [
-    {
-        id: 1,
-        text: 'Doctors Appointment',
-        day: 'Feb 5th at 2.30pm',
-        reminder: false,
-    },
-
-    {
-        id: 2,
-        text: 'Grocery Shopping',
-        day: 'Mar 21st at 4.00pm',
-        reminder: false,
-    }
-]
-
-const Tasks = () => {
+const Tasks = ({ tasks }) => {
     return (
         <>
           {tasks.map((task) => (
-            <h3>{task.text}</h3>
+            <h3 key={task.id}>{task.text}</h3>
         ))}  
         </>
     )
