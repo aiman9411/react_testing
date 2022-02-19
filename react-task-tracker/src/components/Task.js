a3fc37e50eeff754bbec0f10cd5cfa44fc0c34f7
Aiman Nazmi <aimannazmi59@gmail.com>
2022-02-19 13:21:23 +0800

    add deleteTask function

--- react-task-tracker/src/components/Task.js
+++ react-task-tracker/src/components/Task.js
@@ -5,7 +5,7 @@ function Task( { task, onDelete } ) {
         <div className="task">
             <h3>
                 {task.text} 
-                <FaTimes style={{color: 'red', cursor: 'pointer'}} onClick={onDelete} />
+                <FaTimes style={{color: 'red', cursor: 'pointer'}} onClick={() => onDelete(task.id)} />
             </h3>
             <p>{task.day}</p>
         </div>
